069ffdbc4ea65e2bc7ede453856a5eb7a3c3d55c
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-03-25 18:50:31 +0900

    Add: 아이템 스크립트 추가

---
+++ Assets/Scripts/Item/ItemObject.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    public ItemData data;
+
+    public string GetInteractPrompt()
+    {
+        string str = $"{data.displayName}\n{data.description}";
+        return str;
+    }
+
+    public void OnInteract()
+    {
+        InventoryManager.Instance.AddItem(data); 
+        Destroy(gameObject);
+    }
+}
